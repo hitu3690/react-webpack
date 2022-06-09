ecbeafe15cd3faed3b915ac22e4b41f1dcd446c0
Yosuke Iida <hitu3690@icloud.com>
2022-06-09 12:42:27 +0900

    registry 登録

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { DefaultTypelessProvider } from "typeless";
+import { DefaultTypelessProvider, Registry, TypelessContext } from "typeless";
 import { MainModule } from "./features/main/module";
 
+const registry = new Registry();
+
 ReactDOM.render(
-  <DefaultTypelessProvider>
+  <TypelessContext.Provider value={{ registry }}>
     <MainModule />
-  </DefaultTypelessProvider>,
+  </TypelessContext.Provider>,
   document.getElementById("root")
 );
