c0bb3c0c09508f23d0fe7ad1d9d0265683a49a30
Yosuke Iida <hitu3690@icloud.com>
2022-06-12 12:30:36 +0900

    typeless router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { DefaultTypelessProvider, Registry, TypelessContext } from "typeless";
+import { Link } from "typeless-router";
 import MainModule from "./features/main/module";
 
 const registry = new Registry();
@@ -8,6 +9,10 @@ export const App: React.FC = () => {
   return (
     <TypelessContext.Provider value={{ registry }}>
       <MainModule />
+      <br />
+      <Link href="/page-a">page a</Link> | <Link href="/page-b">page b</Link>
+      <br />
+      Current location: {location.pathname}
     </TypelessContext.Provider>
   );
 };
