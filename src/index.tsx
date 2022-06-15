749c5a384466d9c314a053d90e166d2abe786097
Yosuke Iida <hitu3690@icloud.com>
2022-06-15 12:46:40 +0900

    HMR

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "../src/App";
+import { Hmr, Registry, startHmr, TypelessContext } from "typeless";
+import { Link } from "typeless-router";
+import MainModule from "./features/main/module";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const registry = new Registry();
+
+const MOUNT_NODE = document.getElementById("root");
+const render = () => {
+  ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+  ReactDOM.render(
+    <Hmr>
+      <TypelessContext.Provider value={{ registry }}>
+        <MainModule />
+        <br />
+        <Link href="/page-a">page a</Link> | <Link href="/page-b">page b</Link>
+        <br />
+        Current location: {location.pathname}
+      </TypelessContext.Provider>
+    </Hmr>,
+    MOUNT_NODE
+  );
+};
+
+if (module.hot) {
+  module.hot.accept(() => {
+    startHmr();
+    render();
+  });
+}
+render();
